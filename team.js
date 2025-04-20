8b85054b4d701ab8a353d6fbe7e78d901854bebe
KwakkNamHo <wkqlejrhtynh@gmail.com>
2025-04-20 19:48:39 +0900

    push

--- team.js
+++ team.js
@@ -14,28 +14,28 @@ const cardDetails = [
     title: "👤 김태빈",
     description:
       "학과: 정보보호학과<br>학번: 92212790<br>연락처 : 010-7221-7727",
-    link: "https://github.com/taebin327",
+    link: "https://private-homepage-pi",
   },
   {
     title: "👤 이원준",
     description:
       "학과: 정보보호학과<br>학번 : 92416140<br>연락처 : 010-4841-6929",
-    link: "https://github.com/joon-afk",
+    link: "https://portfolio-lee-won-joons-projects.vercel.app/ ",
   },
   {
     title: "👤 김민우",
     description:
       "학과: 정보보호학과<br>학번 : 92212752<br>연락처 : 010-8700-9404",
-    link: "https://github.com/vmaca123",
+    link: "https://portfolio-m497.vercel.app/",
   },
   {
     title: "👤 곽남호",
     description:
       "학과: 정보보호학과<br>학번: 92212697<br>연락처 : 010-7454-3904",
-    link: "https://github.com/KwakNamHo",
+    link: "https://namho-portfolio.vercel.app/",
   },
   {
-    title: "Team Project",
+    title: "보안과 암호학의 세계",
     description:
       "보안과 암호학의 기초 개념을 알아보고, 다양한 체험과 정보를 제공합니다. ",
     link: null,
